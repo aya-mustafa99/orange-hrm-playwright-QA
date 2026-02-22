19fcbe2161f9d07e743ad8ceb0b688ec3be8e1ea
aya-mustafa99 <ayaa.qrinawi@gmail.com>
2026-02-22 10:38:04 +0200

    Update file

--- tests/test_add_employee.py
+++ tests/test_add_employee.py
@@ -1,37 +1,27 @@
 import re
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Page, expect
 
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",wait_until="networkidle",
-    timeout=120000)
+def test_example(page: Page) -> None:
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    page.get_by_role("textbox", name="Username").click()
     page.get_by_role("textbox", name="Username").fill("Admin")
     page.get_by_role("textbox", name="Password").click()
     page.get_by_role("textbox", name="Password").fill("admin123")
     page.get_by_role("button", name="Login").click()
     page.get_by_role("link", name="PIM").click()
-    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList")
     page.get_by_role("button", name=" Add").click()
     page.get_by_role("textbox", name="First Name").click()
     page.get_by_role("textbox", name="First Name").fill("ayaa")
-    page.get_by_role("textbox", name="First Name").press("ArrowRight")
     page.get_by_role("textbox", name="Middle Name").click()
     page.get_by_role("textbox", name="Middle Name").fill("mustafa")
     page.get_by_role("textbox", name="Last Name").click()
     page.get_by_role("textbox", name="Last Name").fill("qrinawi")
     page.get_by_role("textbox").nth(4).click()
-    page.get_by_role("textbox").nth(4).fill("5555555")
+    page.get_by_role("textbox").nth(4).click()
+    page.get_by_role("textbox").nth(4).fill("444444")
     page.get_by_role("button", name="Save").click()
-    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/210")
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/193")
     expect(page.locator("#app")).to_contain_text("Personal Details")
-
-    # ---------------------
-    context.close()
-    browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
+    expect(page.locator("#app")).to_contain_text("Save")
+    page.locator("form").filter(has_text="Employee Full NameEmployee").get_by_role("button").click()
