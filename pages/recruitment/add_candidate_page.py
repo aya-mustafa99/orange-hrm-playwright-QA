4723c6b4754572f4dfd825537ddc4c336ef5200f
aya-mustafa99 <ayaa.qrinawi@gmail.com>
2026-04-18 21:39:22 +0300

    add api fixtures

--- pages/recruitment/add_candidate_page.py
+++ pages/recruitment/add_candidate_page.py
@@ -13,6 +13,7 @@ class CandidateData:
     vacancy: str = ""
     contact_number: str = field(default_factory=lambda: fake.numerify("059#######"))
     email: str = field(default_factory=fake.email)
+    resume_path: str = "tests/files/resume.pdf"  
 
 
 class AddCandidatePage:
@@ -26,7 +27,11 @@ class AddCandidatePage:
         self.contact_number_input = page.get_by_label("Contact Number")
         self.email_input          = page.get_by_label("Email")
         self.consent_checkbox     = page.get_by_label("Consent to keep data")
+        self.resume_input         = page.locator('input[type="file"]')
         self.save_button          = page.get_by_role("button", name="Save")
+
+    def upload_resume(self, file_path):
+        self.resume_input.set_input_files(file_path)
 
     def add_candidate(self, candidate: CandidateData):
         self.first_name_input.fill(candidate.first_name)
@@ -38,5 +43,6 @@ class AddCandidatePage:
 
         self.contact_number_input.fill(candidate.contact_number)
         self.email_input.fill(candidate.email)
+        self.upload_resume(candidate.resume_path)
         self.consent_checkbox.check()
         self.save_button.click()
--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,32 @@ def login(page: Page):
     login_page.login("Admin", "admin123")
     return page
 
+@pytest.fixture
+def employee_data():
+    return {
+        "first_name": "John",
+        "last_name": "Smith",
+        "full_name": "John Smith"
+    }
+
+
+@pytest.fixture
+def vacancy_data(employee_data):
+    return {
+        "name": "Automation Engineer",
+        "job_title": "QA Engineer",
+        "hiring_manager": employee_data["full_name"]
+    }
+
+
+@pytest.fixture
+def candidate_data(vacancy_data):
+    return {
+        "first_name": "Ali",
+        "middle_name": "QA",
+        "last_name": "Test",
+        "email": "ali.test@example.com",
+        "contact": "0591234567",
+        "vacancy": vacancy_data["name"],
+        "resume_path": "tests/files/resume.pdf"
+    }
